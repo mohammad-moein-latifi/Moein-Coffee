6b7d4fb774340f19e52bd6b4daf7b31d7aca7a0d
mohammad-moein-latifi <mohammad.moein.latifi@gmail.com>
2025-08-29 23:15:05 +0330

    style(api/auth): normalize quotes in logout handler

--- src/app/api/auth/signout/route.js
+++ src/app/api/auth/signout/route.js
@@ -1,9 +1,9 @@
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers';
 
 export async function POST() {
-  cookies().delete("token");
+  cookies().delete('token');
   return Response.json(
-    { message: "User logged out successfully" },
+    { message: 'User logged out successfully' },
     { status: 200 }
   );
 }
