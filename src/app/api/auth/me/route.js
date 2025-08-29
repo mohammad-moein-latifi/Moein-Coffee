426a0dcc542235b6af053fddf904d469c4104fa6
mohammad-moein-latifi <mohammad.moein.latifi@gmail.com>
2025-08-29 23:13:14 +0330

    refactor(api/auth): normalize cookie handling and wrap response under data

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -1,16 +1,18 @@
-import connectToDB from "@/configs/db";
-import { verifyAccessToken } from "@/utils/auth";
-import { cookies } from "next/headers";
-import UserModel from "@/models/User";
+import UserModel from '@/models/User';
+import connectToDB from '@/configs/db';
+import { cookies } from 'next/headers';
+import { verifyAccessToken } from '@/utils/auth';
 
 export async function GET() {
   try {
     await connectToDB();
 
-    const token = cookies().get("token");
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token');
+
     if (!token) {
       return Response.json(
-        { data: null, message: "Access token not provided" },
+        { data: null, message: 'Access token not provided' },
         { status: 401 }
       );
     }
@@ -18,27 +20,27 @@ export async function GET() {
     const tokenPayload = verifyAccessToken(token.value);
     if (!tokenPayload) {
       return Response.json(
-        { data: null, message: "Invalid or expired token" },
+        { data: null, message: 'Invalid or expired token' },
         { status: 401 }
       );
     }
 
     const user = await UserModel.findOne(
       { email: tokenPayload.email },
-      "-password"
+      '-password'
     );
 
     if (!user) {
       return Response.json(
-        { data: null, message: "User not found" },
+        { data: null, message: 'User not found' },
         { status: 404 }
       );
     }
 
-    return Response.json(user, { status: 200 });
+    return Response.json({ data: user }, { status: 200 });
   } catch (err) {
     return Response.json(
-      { message: "Internal server error", error: err.message },
+      { message: 'Internal server error', error: err.message },
       { status: 500 }
     );
   }
