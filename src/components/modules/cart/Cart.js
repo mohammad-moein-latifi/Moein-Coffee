b88f0d06d61bdec751d423211adc28bc638c3509
mohammad-moein-latifi <mohammad.moein.latifi@gmail.com>
2025-08-30 13:39:49 +0330

    feat(components): create Cart component with mobile and desktop variations

---
+++ src/components/modules/cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MobileCart from './MobileCart';
+import DesktopCart from './DesktopCart';
+
+export default function Cart() {
+  return (
+    <>
+      <MobileCart />
+      <DesktopCart />
+    </>
+  );
+}
