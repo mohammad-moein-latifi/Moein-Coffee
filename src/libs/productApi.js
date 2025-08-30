6c3f102a1dbf5da7bdb36ff075d874e64715f891
mohammad-moein-latifi <mohammad.moein.latifi@gmail.com>
2025-08-30 12:27:53 +0330

    chore(libs): simple getProducts helper pointing to API

---
+++ src/libs/productApi.js
@@ -0,0 +1,11 @@
+export async function getProducts() {
+  const res = await fetch('http://localhost:3000/api/products', {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
+  return res.json();
+}
