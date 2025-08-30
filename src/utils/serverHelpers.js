fbc7a168a65b35f1a786b33db153d51a7f465346
mohammad-moein-latifi <mohammad.moein.latifi@gmail.com>
2025-08-30 10:50:41 +0330

    refactor(utils/serverHelpers): simplify authUser/authAdmin and use cookieStore

--- src/utils/serverHelpers.js
+++ src/utils/serverHelpers.js
@@ -1,37 +1,34 @@
-import { cookies } from 'next/headers';
 import UserModel from '@/models/User';
+import { cookies } from 'next/headers';
 import connectToDB from '@/configs/db';
 import { verifyAccessToken } from './auth';
 
 const authUser = async () => {
   await connectToDB();
-  const token = cookies().get('token');
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get('token');
   if (!token) return null;
 
   const tokenPayload = verifyAccessToken(token.value);
   if (!tokenPayload) {
-    console.error('❌ Invalid access token.');
     return null;
   }
 
   const user = await UserModel.findOne({ email: tokenPayload.email });
   if (!user) {
-    console.error('❌ User not found.');
     return null;
   }
 
-  console.log('✅ Authenticated user:', user.email);
   return user;
 };
 
 const authAdmin = async () => {
   const user = await authUser();
   if (user && user.role === 'ADMIN') {
-    console.log('✅ Admin authenticated:', user.email);
     return user;
   }
 
-  console.error('❌ Access denied. Admin only.');
   return null;
 };
 
