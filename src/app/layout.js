68a212267597d1f7b5f2bb8c21a2d9724b363faa
mohammad-moein-latifi <mohammad.moein.latifi@gmail.com>
2025-08-30 10:46:37 +0330

    feat(ui/layout): move RootLayout into main and add Providers + ToastContainer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,8 @@
+import Providers from './providers';
+import ClientOnly from '@/components/common/ClientOnly';
+
 import './globals.css';
+import { ToastContainer } from 'react-toastify';
 
 export const metadata = {
   title: 'Moein Coffee | خرید قهوه تازه و باکیفیت',
@@ -22,7 +26,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className="font-vazir bg-gray-100 dark:bg-zinc-800 overflow-x-hidden">
-        {children}
+        <Providers>
+          <ClientOnly>{children}</ClientOnly>
+        </Providers>
+
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          closeOnClick={false}
+          rtl
+          pauseOnHover
+          pauseOnFocusLoss={false}
+          newestOnTop
+          toastClassName="toast-root max-w-sm w-full flex items-start gap-3 rounded-xl shadow-lg border px-4 py-3 bg-stone-900/80 text-stone-100"
+          bodyClassName="font-vazir-md text-sm leading-tight"
+          progressClassName="bg-amber-400"
+          closeButton={false}
+        />
       </body>
     </html>
   );
